8ebfd9582cb829a39a9320d6bdf3e07d1eab153a
David Tatti <lespaul80@hotmail.com>
2014-09-10 13:28:21 +0200

    moved FileWidget and DragAndDropWidget to framework from admin-client common

--- javascripts/oceanfront/oceanfront.js
+++ javascripts/oceanfront/oceanfront.js
@@ -3815,3 +3815,314 @@ var Delimiter = Widget.extend({
     this.setStyleName("delimiter");
   }
 });
+
+var DragAndDropWidget = FlowPanel.extend({
+  init: function() {
+    this._super();
+    this.clickListeners = [];
+    this.focusListeners = [];
+    this.keyboardListeners = [];
+    this.loadListeners = [];
+    this.loadendListeners = [];
+    this.loadstartListeners = [];
+    this.progressListeners = [];
+    this.abortListeners = [];
+    this.errorListeners = [];
+    this.dragstartListeners = [];
+    this.dragListeners = [];
+    this.dragenterListeners = [];
+    this.dragleaveListeners = [];
+    this.dragoverListeners = [];
+    this.dropListeners = [];
+    this.dragendListeners = [];
+    this.sinkEvents(Event.ONCLICK|Event.FOCUSEVENTS|Event.KEYEVENTS|Event.ONLOAD|Event.ONLOADEND|Event.ONLOADSTART|Event.ONPROGRESS|Event.ONABORT|Event.ONERROR|Event.ONDRAGSTART|Event.ONDRAG|Event.ONDRAGENTER|Event.ONDRAGLEAVE|Event.ONDRAGOVER|Event.ONDROP|Event.ONDRAGEND);
+  },
+  addLoadListener: function(listener) {
+    this.loadListeners.push(listener);
+    return this;
+  },
+  addLoadStartListener: function(listener) {
+    this.loadstartListeners.push(listener);
+    return this;
+  },
+  addLoadEndListener: function(listener) {
+    this.loadendListeners.push(listener);
+    return this;
+  },
+  addProgressListener: function(listener) {
+    this.progressListeners.push(listener);
+    return this;
+  },
+  addAbortListener: function(listener) {
+    this.abortListeners.push(listener);
+    return this;
+  },
+  addErrorListener: function(listener) {
+    this.errorListeners.push(listener);
+    return this;
+  },
+  addClickListener: function(listener) {
+    this.clickListeners.push(listener);
+    return this;
+  },
+  addFocusListener: function(listener) {
+    this.focusListeners.push(listener);
+    return this;
+  },
+  addKeyboardListener: function(listener) {
+    this.keyboardListeners.push(listener);
+    return this;
+  },
+  addDragStartListener: function(listener) {
+    this.dragstartListeners.push(listener);
+    return this;
+  },
+  addDragListener: function(listener) {
+    this.dragListeners.push(listener);
+    return this;
+  },
+  addDragEnterListener: function(listener) {
+    this.dragenterListeners.push(listener);
+    return this;
+  },
+  addDragLeaveListener: function(listener) {
+    this.dragleaveListeners.push(listener);
+    return this;
+  },
+  addDragOverListener: function(listener) {
+    this.dragoverListeners.push(listener);
+    return this;
+  },
+  addDropListener: function(listener) {
+    this.dropListeners.push(listener);
+    return this;
+  },
+  addDragEndListener: function(listener) {
+    this.dragendListeners.push(listener);
+    return this;
+  },
+  onBrowserEvent: function(event) {
+    var type = DOM.eventGetType(event);
+
+    if (type == 'click') {
+      for (var i = 0; i < this.clickListeners.length; i++) {
+        this.clickListeners[i](this, event);
+      }
+    }
+    else if (type == 'blur' || type == 'focus') {
+      for (var i = 0; i < this.focusListeners.length; i++) {
+        this.focusListeners[i](this, event);
+      }
+    }
+    else if (type == 'keydown' || type == 'keypress' || type == 'keyup') {
+      for (var i = 0; i < this.keyboardListeners.length; i++) {
+        this.keyboardListeners[i](this, event);
+      }
+    }
+    else if (type == 'load') {
+      for (var i = 0; i < this.loadListeners.length; i++) {
+        this.loadListeners[i](this, event);
+      }
+    }
+    else if (type == 'loadend') {
+      for (var i = 0; i < this.loadendListeners.length; i++) {
+        this.loadendListeners[i](this, event);
+      }
+    }
+    else if (type == 'progress') {
+      for (var i = 0; i < this.progressListeners.length; i++) {
+        this.progressListeners[i](this, event);
+      }
+    }
+    else if (type == 'abort') {
+      for (var i = 0; i < this.abortListeners.length; i++) {
+        this.abortListeners[i](this, event);
+      }
+    }
+    else if (type == 'error') {
+      for (var i = 0; i < this.errorListeners.length; i++) {
+        this.errorListeners[i](this, event);
+      }
+    }
+    else if (type == 'drop') {
+      for (var i = 0; i < this.dropListeners.length; i++) {
+        this.dropListeners[i](this, event);
+      }
+    }
+    else if (type == 'dragenter') {
+      for (var i = 0; i < this.dragenterListeners.length; i++) {
+        this.dragenterListeners[i](this, event);
+      }
+    }
+    else if (type == 'dragleave') {
+      for (var i = 0; i < this.dragleaveListeners.length; i++) {
+        this.dragleaveListeners[i](this, event);
+      }
+    }
+    else if (type == 'dragend') {
+      for (var i = 0; i < this.dragendListeners.length; i++) {
+        this.dragendListeners[i](this, event);
+      }
+    }
+    else if (type == 'dragstart') {
+      for (var i = 0; i < this.dragstartListeners.length; i++) {
+        this.dragstartListeners[i](this, event);
+      }
+    }
+    else if (type == 'dragover') {
+      for (var i = 0; i < this.dragoverListeners.length; i++) {
+        this.dragoverListeners[i](this, event);
+      }
+    }
+  }
+});
+
+var FileWidget = DragAndDropWidget.extend({
+  init: function() {
+    this._super();
+    this.setStyleName("fileWidget");
+    this.render();
+    this.filedata = null;
+    this.data = {};
+  },
+  setText: function(text) {
+    // Must add text container that's over image area, since innerHTML will be set thus erasing the IMG tag currently
+    //this.droptext = text;
+    //DOM.setInnerText(this.imageHolder.getElement(),text);
+  },
+  showText: function(show) {
+    if(show) {
+      this.imageLabel.removeStyleName("hide");
+    } else {
+      this.imageLabel.setStyleName("hide");
+    }
+  },
+  clear: function() {
+    if(this.imageHolder.getChildren()[0]) {
+      this.imageHolder.remove(this.imageHolder.getChildren()[0]);
+      delete this.previewImage;
+    }
+    $(this.imageHolder.getElement()).removeClass("fileWidgetFull").addClass("fileWidgetEmpty");
+    this.imageNameL.setText("No file choosen...");
+    this.imageSizeL.setText("---");
+    this.showText(true);
+  },
+  setImgSrc: function(src) {
+    var self = this;
+    if(!this.previewImage) this.createPreview();
+    this.showText(false);
+    this.previewImage.setUrl(src);
+
+    // Calculate real image size
+    var tmpImage = html.img({'src':src});
+    $(tmpImage).on('load', function(e) {
+      // Set Image size and show it
+      self.data.imageWidth = tmpImage.width;
+      self.data.imageHeight = tmpImage.height;
+      self.imageSizeL.setText(self.data.imageWidth + " x " + self.data.imageHeight);
+    });
+  },
+  createPreview: function() {
+    var self = this;
+    this.previewImage = new WImage();
+    this.previewImage.setStyleName("fileWidgetImage");
+    this.imageHolder.setStyleName("fileWidgetFull");
+    this.imageHolder.removeStyleName("fileWidgetEmpty");
+    this.imageHolder.add(this.previewImage);
+  },
+  setMimeType: function(data) {
+    this.data.dataMimeType = data;
+  },
+  setResult: function(data) {
+    this.data.dataResult = data;
+  },
+  setFileName: function(data) {
+    this.data.dataFileName = data;
+    this.imageNameL.setText(data);
+  },
+  setByteSize: function(data) {
+    this.data.dataByteSize = data;
+  },
+  getMimeType: function() {
+    return this.data.dataMimeType;
+  },
+  getResult: function() {
+    if(this.data.dataResult) {
+      return this.data.dataResult;
+    } else {
+      return null;
+    }
+  },
+  getFileName: function() {
+    return this.data.dataFileName;
+  },
+  getByteSize: function() {
+    return this.data.dataByteSize;
+  },
+  getImageSize: function() {
+    return {'width': this.data.imageWidth,
+            'height': this.data.imageHeight};
+  },
+  render: function() {
+    var self = this;
+    this.imageHolder = new FlowPanel();
+    this.imageLabel = new Text("Drop image here!");
+    this.imageNameL = new Text("No file choosen...");
+    this.imageSizeL = new Text("---");
+    this.fileB = new FileBox();
+    this.fileB.addChangeListener(function(widget, e) {
+      e.stopPropagation();
+      e.preventDefault();
+
+      var files = e.target.files;
+      for (var i = 0, f; f = files[i]; i++) {
+
+        // Only process image files.
+        if (!f.type.match('image.*')) {
+          continue;
+        }
+
+        if(console) console.log("Reading file:");
+        if(console) console.log(f);
+
+        var reader = new FileReader();
+
+        reader.onload = (function(file) {
+          return function(e) {
+            //src in WImage should have value: "data:image/jpg;base64,DATA"
+            self.setMimeType(file.type);
+            self.setResult(btoa(e.target.result));
+            self.setFileName(file.name);
+            self.setByteSize(file.size);
+            self.setImgSrc("data:"+file.type +";base64," + btoa(e.target.result));
+          };
+        })(f);
+        reader.readAsBinaryString(f);
+      }
+    });
+    $(this.imageHolder.getElement()).bind('dragenter', function(e) {
+      if(!self.previewImage) $(e.target).addClass("fileWidgetFull").removeClass("fileWidgetEmpty");
+    });
+    $(this.imageHolder.getElement()).bind('dragleave', function(e) {
+      if(!self.previewImage) $(e.target).removeClass("fileWidgetFull").addClass("fileWidgetEmpty");
+    });
+
+    this.fileB.setStyleName("fileWidgetInputButton");
+    
+    this.imageHolder.setStyleName("fileWidgetImageHolder fileWidgetEmpty");
+    this.imageLabel.setStyleName("fileWidgetImageLabel");
+    this.imageNameL.setStyle("margin", "5px 0px 0px 0px");
+    this.imageNameL.setStyle("font-size", "12px");
+    this.imageNameL.setWidth("200px");
+
+    this.imageSizeL.setStyle("margin", "5px 0px 0px 0px");
+    this.imageSizeL.setStyle("font-size", "10px");
+    this.imageSizeL.setWidth("200px");
+
+    this.add(this.imageHolder);
+    this.add(this.fileB);
+    this.add(this.imageLabel);
+    this.add(this.imageNameL);
+    this.add(this.imageSizeL);
+  }
+});
